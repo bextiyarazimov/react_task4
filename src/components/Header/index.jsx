5fbe2cc41737958604568ac4aedcc2a16f55aabd
Baxtiyar Azimov <baxtiyar.azimov@aahm.lan>
2024-02-13 11:37:27 +0400

    add

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import styles from './header.module.css';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from 'react';
+
+export function Header({ handleAddTask }) {
+  const [title, setTitle] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleAddTask(title);
+    setTitle('');
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <h1>TODO</h1>
+      <img src="src/assets/Combined Shape.png" alt="Logo" />
+
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input placeholder="Add a new task" type="text" onChange={onChangeTitle} value={title} />
+        <button>Create <AiOutlinePlusCircle size={20} /></button>
+      </form>
+    </header>
+  )
+}
